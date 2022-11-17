3a0c07062394b00bce8cfdf908f25523aa712db0
Shad0wz93 <Shadowz9@icloud.com>
2022-11-17 14:36:44 +0100

    nul

--- delete.php
+++ delete.php
@@ -1,4 +1,4 @@
-<?php require 'connect.php'; $id=0; if(!empty($_GET['id'])){ $id=$_REQUEST['id']; } if(!empty($_POST)){ $id= $_POST['id']; $pdo=Database::connect(); $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?php require 'connect.php'; $id=0; if(!empty($_GET['id'])){ $id=$_REQUEST['id']; } if(!empty($_POST)){ $id= $_POST['id']; $dbh=Database::connect(); $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      $sql = "DELETE FROM livres  WHERE id = ?";
         $q = $dbh->prepare($sql);
         $q->execute(array($id));
@@ -13,7 +13,7 @@
 <head>
     <meta charset="utf-8">
     	<link href="css/bootstrap.min.css" rel="stylesheet">
-    <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-wp-preserve="%3Cscript%20src%3D%22js%2Fbootstrap.min.js%22%3E%3C%2Fscript%3E" data-mce-resize="false" data-mce-placeholder="1" class="mce-object" width="20" height="20" alt="<script>" title="<script>" />
+    
 </head>
  
 <body>
